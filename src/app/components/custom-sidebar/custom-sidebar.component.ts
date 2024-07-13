59899e9dd635c62668a6a39a3cca6ddb4bcd9719
MamaneHassane <hassane.mamane017@gmail.com>
2024-07-13 14:21:02 +0100

    Will do the project with ant design ng-zorro

--- src/app/components/custom-sidebar/custom-sidebar.component.ts
+++ src/app/components/custom-sidebar/custom-sidebar.component.ts
@@ -1,28 +1,18 @@
-import {Component, OnInit, WritableSignal} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Button } from "primeng/button";
-import { MenuItem } from "primeng/api";
-import menuItems from '../constants/MenuItems';
-import {SidebarModule} from "primeng/sidebar";
 
 @Component({
   selector: 'app-custom-sidebar',
   standalone: true,
   imports: [
     RouterLink,
-    Button,
-    SidebarModule
   ],
   templateUrl: './custom-sidebar.component.html',
   styleUrl: './custom-sidebar.component.css'
 })
-export class CustomSidebarComponent implements OnInit{
-  items: MenuItem[] | undefined;
-  sidebarVisible: boolean = true;
-
+export class CustomSidebarComponent implements OnInit {
   ngOnInit(){
-    this.items = menuItems
-    console.log(`The items are ${JSON.stringify(this.items)}`)
+
   }
 
 }
